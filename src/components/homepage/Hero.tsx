4cdf8c27d7c8b2b2f04d292463a01ea5c003620c
Sanjay2929 <Sanjayjyani220@gmail.com>
2024-01-29 15:18:48 +0530

    hero animation

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center'>
-      <h1 className='text-red-600 text-[50px] font-extrabold uppercase'>Welcome Home</h1>
+    <div className='min-h-screen flex justify-center items-center w-full hero'>
+      <span className='text-red-600 text-[50px] font-extrabold uppercase hero_animation whitespace-nowrap overflow-hidden'>Welcome Home</span>
     </div>
   );
 }
